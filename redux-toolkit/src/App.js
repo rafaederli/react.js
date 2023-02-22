8ba46c3a78b9d54cb5208199e5ee3e8fe7472c92
Rafael Ederli <rafaederli@gmail.com>
2023-02-22 09:51:06 -0300

    adding a new feature: posts

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,9 +1,11 @@
-import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
   return (
     <main className="App">
-      <Counter />
+      <AddPostForm />
+      <PostsList />
     </main>
   );
 }
